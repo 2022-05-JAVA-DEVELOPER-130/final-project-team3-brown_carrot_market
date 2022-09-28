d614e5f9f5ca217eda5032dfaacee846db0ee9ca
yoon <yoon@localhost>
2022-09-28 12:02:05 +0900

    ..

---
+++ src/main/java/com/itwill/brown_carrot_market/BrownCarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package com.itwill.brown_carrot_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownCarrotMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrownCarrotMarketApplication.class, args);
+	}
+
+}
