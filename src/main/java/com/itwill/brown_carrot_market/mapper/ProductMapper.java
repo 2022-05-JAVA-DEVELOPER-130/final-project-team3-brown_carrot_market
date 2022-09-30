37cb9d39b7f0771be305359d6875bfd6ad9aa14f
minjoo <ignore4557@gmail.com>
2022-09-30 14:42:58 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/mapper/ProductMapper.java
+++ src/main/java/com/itwill/brown_carrot_market/mapper/ProductMapper.java
@@ -1,5 +1,9 @@
 package com.itwill.brown_carrot_market.mapper;
 
-public interface ProductMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductMapper {
 }
