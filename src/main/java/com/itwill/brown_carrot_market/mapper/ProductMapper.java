2474cec2b03b029753d6ecf78561e66df5edd2bc
minjoo <ignore4557@gmail.com>
2022-09-30 15:16:01 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/mapper/ProductMapper.java
+++ src/main/java/com/itwill/brown_carrot_market/mapper/ProductMapper.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.itwill.brown_carrot_market.dto.Product;
+
 @Mapper
 public interface ProductMapper {
+	public List<Product> selectProductAll();
+	
+	public List<Product> selectAllByCtgr();
+	
 }
