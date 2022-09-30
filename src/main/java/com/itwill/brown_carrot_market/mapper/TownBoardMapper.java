d1338b8bc74c933f6a061f8e8bb2c75da99826b2
imeunbi <amy980524@naver.com>
2022-09-30 16:21:58 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/mapper/TownBoardMapper.java
+++ src/main/java/com/itwill/brown_carrot_market/mapper/TownBoardMapper.java
@@ -1,42 +1,43 @@
-package com.itwill.brown_carrot_market.mapper;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.itwill.brown_carrot_market.dto.TownBoard;
-
-@Mapper
-public interface TownBoardMapper {
-	
-	public List<TownBoard> selectNonMemberTownBoardList();
-	
-	public List<TownBoard> selectNonMemberCtgrTownBoardList();
-	
-	public List<TownBoard> selectTownBoardListCoordinate();
-	
-	public List<TownBoard> selectTownBoardCtgrListCoordinate(int t_ctgr_no);
-	
-	public TownBoard selectTownBoardOne(int t_no);
-	
-	public int deleteTownBoardOne(int t_no);
-	
-	public int updateTownBoardOne(TownBoard townBoard);
-	
-	public int updateTownBoardCount(int t_no);
-	
-	public int insertTownBoard(TownBoard townBoard);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+/*
+ * package com.itwill.brown_carrot_market.mapper;
+ * 
+ * import java.util.List;
+ * 
+ * import org.apache.ibatis.annotations.Mapper;
+ * 
+ * import com.itwill.brown_carrot_market.dto.TownBoard;
+ * 
+ * @Mapper public interface TownBoardMapper {
+ * 
+ * public List<TownBoard> selectNonMemberTownBoardList();
+ * 
+ * public List<TownBoard> selectNonMemberCtgrTownBoardList();
+ * 
+ * public List<TownBoard> selectTownBoardListCoordinate();
+ * 
+ * public List<TownBoard> selectTownBoardCtgrListCoordinate(int t_ctgr_no);
+ * 
+ * public TownBoard selectTownBoardOne(int t_no);
+ * 
+ * public int deleteTownBoardOne(int t_no);
+ * 
+ * public int updateTownBoardOne(TownBoard townBoard);
+ * 
+ * public int updateTownBoardCount(int t_no);
+ * 
+ * public int insertTownBoard(TownBoard townBoard);
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * }
+ */
