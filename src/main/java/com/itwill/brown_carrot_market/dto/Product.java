8825d176c333eb80c76e98d5921d3db1cef48241
minjoo <ignore4557@gmail.com>
2022-09-30 13:54:54 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/dto/Product.java
+++ src/main/java/com/itwill/brown_carrot_market/dto/Product.java
@@ -1,5 +1,7 @@
 package com.itwill.brown_carrot_market.dto;
 
+import java.util.List;
+
 /*
  이름             널?       유형            
 -------------- -------- ------------- 
@@ -33,12 +35,14 @@ public class Product {
 	public ProductCategory productCategory;
 	public UserInfo userInfo;
 	
+	public List<ProductImage> productImagesList;
+	
 	public Product() {
 	}
 
 	public Product(int p_no, String p_title, String p_desc, int p_price, String p_date, int p_sell, int p_count,
 			int p_wish, String p_address_name, int p_address_lat, int p_address_lng, ProductCategory productCategory,
-			UserInfo userInfo) {
+			UserInfo userInfo,List<ProductImage> productImagesList) {
 		super();
 		this.p_no = p_no;
 		this.p_title = p_title;
@@ -53,6 +57,7 @@ public class Product {
 		this.p_address_lng = p_address_lng;
 		this.productCategory = productCategory;
 		this.userInfo = userInfo;
+		this.productImagesList = productImagesList;
 	}
 
 
@@ -159,12 +164,23 @@ public class Product {
 	public void setUserInfo(UserInfo userInfo) {
 		this.userInfo = userInfo;
 	}
-	
+
+	public List<ProductImage> getProductImagesList() {
+		return productImagesList;
+	}
+
+	public void setProductImagesList(List<ProductImage> productImagesList) {
+		this.productImagesList = productImagesList;
+	}
+
 	@Override
 	public String toString() {
 		return "Product [p_no=" + p_no + ", p_title=" + p_title + ", p_desc=" + p_desc + ", p_price=" + p_price
 				+ ", p_date=" + p_date + ", p_sell=" + p_sell + ", p_count=" + p_count + ", p_wish=" + p_wish
 				+ ", p_address_name=" + p_address_name + ", p_address_lat=" + p_address_lat + ", p_address_lng="
-				+ p_address_lng + ", productCategory=" + productCategory + ", userInfo=" + userInfo + "]";
+				+ p_address_lng + ", productCategory=" + productCategory + ", userInfo=" + userInfo
+				+ ", productImagesList=" + productImagesList + "]";
 	}
+	
+	
 }
