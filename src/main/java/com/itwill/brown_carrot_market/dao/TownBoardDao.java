6e278a89a59debfaf89e10050ece1549619f9523
imeunbi <amy980524@naver.com>
2022-09-30 17:11:44 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/dao/TownBoardDao.java
+++ src/main/java/com/itwill/brown_carrot_market/dao/TownBoardDao.java
@@ -2,6 +2,8 @@ package com.itwill.brown_carrot_market.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.itwill.brown_carrot_market.dto.TownBoard;
 
 public interface TownBoardDao {
@@ -10,7 +12,7 @@ public interface TownBoardDao {
 	int insertTownBoard(TownBoard townBoard);
 	
 	//동네 게시판 글 하나 삭제
-	int deleteTownBoardOne(int t_no);
+	//int deleteTownBoardOne(int t_no);
 	
 	
 	
