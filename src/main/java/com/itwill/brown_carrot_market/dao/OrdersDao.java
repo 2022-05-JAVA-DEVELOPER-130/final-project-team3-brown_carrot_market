d6606d68f5fa896e69acfd961b7e1159686bf353
yoon <yoon@localhost>
2022-10-02 17:02:31 +0900

    orders dto, dao, service 업데이트

--- src/main/java/com/itwill/brown_carrot_market/dao/OrdersDao.java
+++ src/main/java/com/itwill/brown_carrot_market/dao/OrdersDao.java
@@ -1,5 +1,16 @@
 package com.itwill.brown_carrot_market.dao;
 
-public interface OrdersDao {
+import java.util.List;
 
+import com.itwill.brown_carrot_market.dto.Orders;
+
+public interface OrdersDao {
+	
+	int insertOrders(Orders orders) throws Exception;
+	
+	int deleteOrders(int orders_no) throws Exception;
+	
+	Orders selectByNo(int orders_no) throws Exception;
+	
+	List<Orders> selectAllById(String user_id) throws Exception;
 }
--- src/main/java/com/itwill/brown_carrot_market/dao/OrdersDaoImpl.java
+++ src/main/java/com/itwill/brown_carrot_market/dao/OrdersDaoImpl.java
@@ -1,5 +1,53 @@
 package com.itwill.brown_carrot_market.dao;
 
-public class OrdersDaoImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.brown_carrot_market.dto.Orders;
+import com.itwill.brown_carrot_market.mapper.OrdersMapper;
+
+@Repository(value = "ordersDaoImpl")
+public class OrdersDaoImpl implements OrdersDao{
+	@Autowired(required = true)
+	private OrdersMapper ordersMapper;
+	
+	public OrdersDaoImpl() {
+		System.out.println("#### OrdersDaoImplMyBatisMapperInterface() : 디폴트생성자 호출  ");
+	}
+	
+	public OrdersMapper getOrdersMapper() {
+		return ordersMapper;
+	}
+
+	public void setOrdersMapper(OrdersMapper ordersMapper) {
+		this.ordersMapper = ordersMapper;
+	}
+
+	@Override
+	public int insertOrders(Orders orders) throws Exception {
+		System.out.println("#### OrdersDaoImplMyBatisMapperInterface : insertOrders() 호출  ");
+		System.out.println("insertOrders()메소드 : " + ordersMapper);
+		return ordersMapper.insertOrders(orders);
+	}
+
+	@Override
+	public int deleteOrders(int orders_no) throws Exception {
+		System.out.println("#### OrdersDaoImplMyBatisMapperInterface : deleteOrders(int orders_no) 호출  ");
+		return ordersMapper.deleteOrders(orders_no);
+	}
+
+	@Override
+	public Orders selectByNo(int orders_no) throws Exception {
+		System.out.println("#### OrdersDaoImplMyBatisMapperInterface : selectByNo(int orders_no) 호출  ");
+		return ordersMapper.selectByNo(orders_no);
+	}
+
+	@Override
+	public List<Orders> selectAllById(String user_id) throws Exception {
+		System.out.println("#### OrdersDaoImplMyBatisMapperInterface : selectAllById(String user_id) 호출  ");
+		return ordersMapper.selectAllById(user_id);
+	}
 
 }
