729ae1a544c671abcf06a1c565c39dbb6a0ec0ab
amy98 <amy98@BOOK-SNO6NT8Q1L>
2022-10-07 10:35:34 +0900

    공지사항 수정중

--- src/main/java/com/itwill/brown_carrot_market/controller/NoticeController.java
+++ src/main/java/com/itwill/brown_carrot_market/controller/NoticeController.java
@@ -1,5 +1,32 @@
 package com.itwill.brown_carrot_market.controller;
 
-public class NoticeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itwill.brown_carrot_market.dto.Notice;
+import com.itwill.brown_carrot_market.service.NoticeService;
+
+@Controller
+public class NoticeController {
+	@Autowired
+	private NoticeService noticeService;
+	
+	@RequestMapping("/tables")
+	public String notice_list(Model model)throws Exception{
+		List<Notice> noticeList = noticeService.selectAll();
+		model.addAttribute("noticeList", noticeList);
+		
+		return "tables";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
