63f286b85134c0719bd98930cf6d447cefc0b4a8
seoyoung <seoyoung310@gmail.com>
2022-10-07 14:41:36 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/dto/Review.java
+++ src/main/java/com/itwill/brown_carrot_market/dto/Review.java
@@ -2,4 +2,14 @@ package com.itwill.brown_carrot_market.dto;
 
 public class Review {
 
+	/*
+	 이름           널?       유형            
+	------------ -------- ------------- 
+	review_no    NOT NULL NUMBER(10)    
+	review_desc           VARCHAR2(100) 
+	review_image          VARCHAR2(100) 
+	review_point          NUMBER(10,1)  
+	orders_no             NUMBER(10)    
+	user_id               VARCHAR2(30)  
+	 */
 }
