15073f61233f23170cef4a0961af953d44e08184
seoyoung <seoyoung310@gmail.com>
2022-10-07 16:42:56 +0900

    orders Dto하단의 toString수정

--- src/main/java/com/itwill/brown_carrot_market/dao/ReviewDaoImpl.java
+++ src/main/java/com/itwill/brown_carrot_market/dao/ReviewDaoImpl.java
@@ -1,5 +1,7 @@
 package com.itwill.brown_carrot_market.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -7,16 +9,28 @@ import com.itwill.brown_carrot_market.dto.Review;
 import com.itwill.brown_carrot_market.mapper.ReviewMapper;
 
 @Repository(value = "ReviewDaoImpl")
-public class ReviewDaoImpl {
+public class ReviewDaoImpl implements ReviewDao {
 
-	@Autowired(required = false)
+	@Autowired
 	private ReviewMapper reviewMapper;
 	
 	public ReviewDaoImpl() {
 		System.out.println("#### ReviewDaoImpl() : 디폴트생성자 호출");
 	}
 	
+	@Override
 	public int createReview(Review review) {
 		return reviewMapper.createReview(review);
+	}
+
+	@Override
+	public List<Review> findReceivedReview(String user_id) throws Exception {
+		return reviewMapper.findReceivedReview(user_id);
+	}
+
+	@Override
+	public List<Review> findReceivedReviewByBuyer(String user_id)
+			throws Exception {
+		return reviewMapper.findReceivedReviewByBuyer(user_id);
 	}
 }
