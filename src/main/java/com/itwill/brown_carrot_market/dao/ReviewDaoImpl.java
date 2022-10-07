84560ec7ff19fb73db11fb3093a8a5d0505e05cb
seoyoung <seoyoung310@gmail.com>
2022-10-07 15:23:53 +0900

    은비깨비 확인하러 간다잉~

--- src/main/java/com/itwill/brown_carrot_market/dao/ReviewDaoImpl.java
+++ src/main/java/com/itwill/brown_carrot_market/dao/ReviewDaoImpl.java
@@ -1,5 +1,22 @@
 package com.itwill.brown_carrot_market.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.brown_carrot_market.dto.Review;
+import com.itwill.brown_carrot_market.mapper.ReviewMapper;
+
+@Repository(value = "ReviewDaoImpl")
 public class ReviewDaoImpl {
 
+	@Autowired(required = false)
+	private ReviewMapper reviewMapper;
+	
+	public ReviewDaoImpl() {
+		System.out.println("#### ReviewDaoImpl() : 디폴트생성자 호출");
+	}
+	
+	public int createReview(Review review) {
+		return reviewMapper.createReview(review);
+	}
 }
--- src/main/java/com/itwill/brown_carrot_market/mapper/ReviewMapper.java
+++ src/main/java/com/itwill/brown_carrot_market/mapper/ReviewMapper.java
@@ -1,5 +1,12 @@
 package com.itwill.brown_carrot_market.mapper;
 
+import com.itwill.brown_carrot_market.dto.Review;
+
 public interface ReviewMapper {
 
+	/*
+	 * @Insert("")
+	 */
+	public int createReview(Review review);
+	
 }
