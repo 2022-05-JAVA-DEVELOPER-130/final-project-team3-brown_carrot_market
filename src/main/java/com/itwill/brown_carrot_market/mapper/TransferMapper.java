8b4972dced57e435f77f2a11328cb0f5d399f8d6
yoon <yoon@localhost>
2022-10-14 15:17:31 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/mapper/TransferMapper.java
+++ src/main/java/com/itwill/brown_carrot_market/mapper/TransferMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 
@@ -25,6 +26,11 @@ public interface TransferMapper {
 	@Insert("insert into transfer values(TRANSFER_TRANSFER_NO_SEQ.nextval, (select p_price from product where p_no = #{p_no}), 0, sysdate, (select user_id from product where p_no = #{p_no}), (select orders_no from orders where p_no = #{p_no}),#{p_no},0)")
 	int insertTransfer_Withdraw_Cancle(int p_no);
 	
+	@ResultMap(value = { "transferResultMap" })
 	@Select("select s.user_id, s.transfer_no, s.transfer_deposit, s.transfer_withdraw, s.transfer_date, s.t_balance from (select t.user_id, t.transfer_no, t.transfer_deposit, t.transfer_withdraw, t.transfer_date, t.t_balance from transfer t left outer join orders o on t.p_no = o.p_no)s join userinfo u on s.user_id=u.user_id where s.user_id=#{user_id} order by s.transfer_date desc")
 	List<Transfer> selectById(String user_id);
+	
+	@ResultMap(value = { "transferResultMap" })
+	@Select("select p.user_id, p.p_price, p.p_title from transfer t join product p on t.p_no=p.p_no where p.p_no=#{p_no}")
+	List<Transfer> selectByP_No(int p_no);
 }
