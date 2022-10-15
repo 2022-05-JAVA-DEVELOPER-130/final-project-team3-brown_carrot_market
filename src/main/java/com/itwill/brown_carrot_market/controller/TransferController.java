2eb9488f21670801bc6a5c99c22bfbe617c412b6
yoon <yoon@localhost>
2022-10-15 16:04:06 +0900

    ..

--- src/main/java/com/itwill/brown_carrot_market/controller/TransferController.java
+++ src/main/java/com/itwill/brown_carrot_market/controller/TransferController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.brown_carrot_market.dto.Product;
+import com.itwill.brown_carrot_market.dto.ProductImage;
 import com.itwill.brown_carrot_market.dto.Transfer;
 import com.itwill.brown_carrot_market.service.ProductService;
 import com.itwill.brown_carrot_market.service.TransferService;
@@ -38,7 +39,7 @@ public class TransferController {
  
 	@LoginCheck
 	@RequestMapping(value = "/transfer_page")
-	public String transfer_page(int p_no,Model model, HttpServletRequest request) throws Exception {
+	public String transfer_page(int p_no, Model model, HttpServletRequest request) throws Exception {
 		String forwardPath = "";
 		String sUserId = (String) request.getSession().getAttribute("sUserId");
 		Product product = productService.selectByOne(p_no);
@@ -48,10 +49,16 @@ public class TransferController {
 
 	}
 	
-	@RequestMapping(value = "/transfer_action")
+	@RequestMapping(value = "/transfer_complete_action")
 	public String transfer_action(int p_no, Model model, HttpServletRequest request) throws Exception {
 		transferService.transfer_transaction(p_no);
 		return "redirect:transfer_complete?p_no="+p_no;
+	}
+	
+	@RequestMapping(value = "/transfer_cancle_action")
+	public String transfer_cancle_action(int p_no, Model model, HttpServletRequest request) throws Exception {
+		transferService.transfer_transaction(p_no);
+		return "redirect:transfer_cancle?p_no="+p_no;
 	}
 	
 	@LoginCheck
@@ -61,6 +68,15 @@ public class TransferController {
 		Product product = productService.selectByOne(p_no);
 		model.addAttribute("product", product);
 		return "/transfer_complete";
+	}
+	
+	@LoginCheck
+	@RequestMapping(value = "/transfer_cancle")
+	public String transfer_cancle(int p_no,Model model, HttpServletRequest request) throws Exception {
+		String sUserId = (String) request.getSession().getAttribute("sUserId");
+		Product product = productService.selectByOne(p_no);
+		model.addAttribute("product", product);
+		return "/transfer_cancle";
 	}
 
 }
