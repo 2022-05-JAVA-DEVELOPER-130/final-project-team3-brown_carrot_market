7b1559af7b053828f69d34a348b07ff272b33774
qkrrj <qkrrj@DESKTOP-0LUGNVM>
2022-10-18 15:15:47 +0900

    판매완료 -> 판매중, 예약중 변경 불가

--- src/main/webapp/js/chat/chat_room.js
+++ src/main/webapp/js/chat/chat_room.js
@@ -620,8 +620,7 @@ var c ="";
 			console.log('3.로그인id:'+loginId);
 			if(check==1){
 				console.log('세션 = 판매자 : '+loginId);
-				a='<button class="dropdown-item" type="button" id="reserveBtn"><b>예약중으로 변경</b></button>';	
-				b='<button class="dropdown-item" type="button" id="sellBtn"><b>판매중으로 변경</b></button>';	
+				
 				c=`<a href="#" class="btn btn-outline-info" style="display:none"><i class="fa fa-won" id="btnCarrot_Pay" p_no=${product.p_no} style="color:green;"></i></a>`;
 			}
 		}
